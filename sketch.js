ade17e1e7710f48c8b8a08f359bf9dc35fb11aa4
vanshgaur9-max <79321126+vanshgaur9-max@users.noreply.github.com>
2021-03-18 19:24:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+var engine,world;
+function setup(){
+
+  
+
+  var canvas=createCanvas(400,400);
+  engine=Engine.create()
+  world=engine.world;
+  object=Bodies.rectangle(200,100,50,50)
+  World.add(world,object)
+}
+function draw()
+{
+  background(0);
+  Engine.update(engine)
+  rectMode(CENTER);
+    rect(object.position.x,object.position.y,50,50);
+  
+}
